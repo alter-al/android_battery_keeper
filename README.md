15ef5ebdd61060dc21252fc090c26a298f3217b8
Alter-Pesotskiy Alexey <a.alterpesotskiy@corp.mail.ru>
2018-08-17 11:24:46 +0300

    doc and script

---
+++ README.md
@@ -0,0 +1,15 @@
+# android_battery_keeper
+Script that created to save an android battery by maintaining a specific charge.
+
+
+## Required Root Rights
+------
+
+Usage:
+------
+        $ ruby android_battery_keeper.rb
+        // run it on cron (something like one time per 0.5h)
+        
+## License
+
+A tool is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
---
+++ android_battery_keeper.rb
@@ -0,0 +1,40 @@
+@max_battery_level = 42
+@charging_file = '/sys/class/power_supply/battery/charging_enabled'.freeze
+
+def devices
+  `adb devices`.scan(/\n(.*)\t/).flatten
+end
+
+def unplug(udid:)
+  charging = (`adb -s #{udid} shell "su -c 'cat #{@charging_file}'"`.to_i == 1)
+  puts "🔋 Charging enabled: #{charging}"
+  return unless charging
+  puts "🔌 Unplug #{udid}"
+  `adb -s #{udid} shell "su -c 'echo 0 > #{@charging_file}'"`
+end
+
+def plug(udid:)
+  charging = (`adb -s #{udid} shell "su -c 'cat #{@charging_file}'"`.to_i == 1)
+  puts "🔋 Charging enabled: #{charging}"
+  return if charging
+  puts "🔌 Plug #{udid}"
+  `adb -s #{udid} shell "su -c 'echo 1 > #{@charging_file}'"` unless charging
+end
+
+def battery(udid:)
+  battery_level = /level: (.*?)\n/.match(`adb -s #{udid} shell dumpsys battery`)
+  raise("⚠️ Could not get battery level from: #{udid}") if battery_level[1].nil?
+  puts "💡 Battery level: #{battery_level[1]}"
+  battery_level[1].to_i
+end
+
+def battery_saver
+  devices.each do |udid|
+    next if udid.include?('emulator')
+    puts "📱 #{udid}"
+    battery_level = battery(udid: udid)
+    battery_level > @max_battery_level ? unplug(udid: udid) : plug(udid: udid)
+  end
+end
+
+battery_saver
